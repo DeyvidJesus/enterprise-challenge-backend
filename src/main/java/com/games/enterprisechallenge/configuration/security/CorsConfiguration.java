da355923fd7f088a3850c0c63e08f2f8a658d7b0
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2024-10-25 21:52:47 -0300

    fix: CORS config

--- src/main/java/com/games/enterprisechallenge/configuration/security/CorsConfiguration.java
+++ src/main/java/com/games/enterprisechallenge/configuration/security/CorsConfiguration.java
@@ -12,8 +12,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedOrigins("https://enterprise-challenge-frontend.vercel.app/")
+                .allowedHeaders("*");
     }
 }
